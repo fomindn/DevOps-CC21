6ba65516a81aaedee2ea400c5176ae829c9b3620
Dmytro Fomin <fomindn@gmail.com>
2022-01-31 19:45:10 +0200

    Minor changes in .howto files and created Dockerfiles for every docker web service image.

---
+++ Fomin_Dmytro/01.Docker/Dockerfiles/Dockerfile.php
@@ -0,0 +1,5 @@
+FROM php:7.4-apache
+LABEL maintainer="fomindn@gmail.com"
+LABEL version="7.4-apache"
+LABEL description="This PHP v.7.4 docker image is created \
+for the DevOps CI/CD study reason."
