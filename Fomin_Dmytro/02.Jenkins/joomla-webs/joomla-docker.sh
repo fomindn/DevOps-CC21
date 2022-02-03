9a879a0ce4584fbd2bdcb1f3a2372d2b23f329ae
Dmytro Fomin <fomindn@gmail.com>
2022-02-03 14:00:23 +0200

    Made bash script file for docker images running/stoping.

--- Fomin_Dmytro/02.Jenkins/joomla-webs/joomla-docker.sh
+++ Fomin_Dmytro/02.Jenkins/joomla-webs/joomla-docker.sh
@@ -4,20 +4,30 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo -e "${WHITE}Select an action:${NC}
-\t(${RED}1${NC})\tStart the dockers with a Joomla CMS (mysql, pma, joomla)
-\t(${RED}2${NC})\tStop the dokers with a Joomla CMS (mysql, pma, joomla)
-\t(${RED}q${NC})\tExit from script"
-echo -en "${WHITE}Enter you choise:${NC} "
-read choise
+if [ -z $1 ] 
+then
+    echo -e "${WHITE}Select an action:${NC}
+    \t(${RED}1${NC})\tStart the dockers with a Joomla CMS (mysql, pma, joomla)
+    \t(${RED}2${NC})\tStop the dokers with a Joomla CMS (mysql, pma, joomla)
+    \t(${RED}q${NC})\tExit from script"
+    echo -en "${WHITE}Enter you choise:${NC} "
+    read choise
+else
+    choise=$1
+fi
+
 case $choise in
-    1)  docker-compose -f joomla-docker-compose.yml up -d
+    1)  #docker-compose -f joomla-docker-compose.yml up -d
+        echo 1
     ;;
-    2)  docker-compose -f joomla-docker-compose.yml stop
+    2)  #docker-compose -f joomla-docker-compose.yml stop
+        echo 2
     ;;
-    q)  exit 0
+    q)  #exit 0
+        echo q
     ;;
-    *)  exit 1
+    *)  #exit 1
+        echo "Something else"
     ;;
 esac
 
